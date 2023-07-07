bcfa44c9ea8fcf94d0e3127289531fcdbfc9b2ae
LAISRCROFT <44514405+LAISRCROFT@users.noreply.github.com>
2023-07-07 16:06:54 -0300

    feat: Basic Informations Box

--- controllers/services/PhotoService.js
+++ controllers/services/PhotoService.js
@@ -1,7 +1,7 @@
 export default class PhotoService {
 
 	getImages() {
-        return fetch('demo/data/photos.json').then(res => res.json())
+        return fetch('..\\assets\\jsons\\photos.js').then(res => res.json())
                 .then(d => d.data);
-    }
+        }
 }
